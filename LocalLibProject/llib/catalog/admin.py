277ac32edf6318e903fd67901dffd49e569d13af
Piyush Ranjan <piyush@Piyushs-MacBook-Air.local>
2024-06-19 16:19:13 +0530

    basic working website

---
+++ LocalLibProject/llib/catalog/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['book', 'status', 'due_back', 'id']
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+class GenreAdmin(admin.ModelAdmin):
+    pass
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
+admin.site.register(Language, LanguageAdmin)
---
+++ LocalLibProject/llib/catalog/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.views import generic
+
+# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    num_fiction = Book.objects.filter(genre__name__icontains = 'fiction').count()
+    num_genres_fiction = Genre.objects.filter(name__icontains = 'fiction').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_fiction': num_fiction,
+        'num_genres_fiction': num_genres_fiction
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 4
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
