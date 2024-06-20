8d50ddbeb4668ed12317e4195fce5f9becd71468
Piyush Ranjan <piyush@Piyushs-MacBook-Air.local>
2024-06-20 16:38:49 +0530

    user authentication added

--- LocalLibProject/llib/catalog/views.py
+++ LocalLibProject/llib/catalog/views.py
@@ -19,13 +19,18 @@ def index(request):
     num_fiction = Book.objects.filter(genre__name__icontains = 'fiction').count()
     num_genres_fiction = Genre.objects.filter(name__icontains = 'fiction').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_fiction': num_fiction,
-        'num_genres_fiction': num_genres_fiction
+        'num_genres_fiction': num_genres_fiction,
+        'num_visits' : num_visits
     }
 
     # Render the HTML template index.html with the data in the context variable
